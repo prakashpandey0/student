401b587f5d9347cae59aa738e2f59331ad31e3c7
prakash <prakash.pandey911@gmail.com>
2017-01-28 23:32:34 +0530

    pages completed Dashboard missing student myfacility etc

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :factualities
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root 'students#index'
+ root 'pages#dashboard'
 end
